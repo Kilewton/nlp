2be26a45d7df34279e864cf3276425417c87bcf1
Killian <killian.durocher@gmail.com>
2022-01-03 16:16:27 +0100

    update footer vfinale

--- streamlit_nlp_steps.py
+++ streamlit_nlp_steps.py
@@ -657,7 +657,7 @@ def layout(*args):
 
     style_hr = styles(
         display="block",
-        margin=px(1, 1),
+        margin=px(1, 1, "auto", "auto"),
         border_style="inset",
         border_width=px(2)
     )
